ef8a3b8fdfcb9668265c8475ff00e7c6657b5dd3
Andy Thompson <andy@andythompson.com.au>
2020-01-08 19:39:54 +1100

    base quiz working, no timer

--- quiz1/main.go
+++ quiz1/main.go
@@ -3,19 +3,43 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 
 	questionData := readQuestionFile()
+	totalMark := askQuestions(questionData)
+	fmt.Println("Total Marks:", totalMark)
+}
+
+func askQuestions(questionData []QuestionLine) int {
+	totalMark := 0
+	reader := bufio.NewReader(os.Stdin)
+	for i := 0; i < len(questionData); i++ {
+		fmt.Println(fmt.Sprintf("What is: %v?", questionData[i].question))
+		answer, _ := reader.ReadString('\n')
+		answer = strings.Replace(answer, "\n", "", -1)
+
+		if answer == questionData[i].answer {
+			fmt.Println("Correct!")
+			totalMark += 1
+		} else {
+			fmt.Println("You Suck!")
+			fmt.Println(questionData[i].answer)
+		}
+	}
+
+	return totalMark
 }
 
 type QuestionLine struct {
-	Question string
-	Answer   string
+	question string
+	answer   string
 }
 
 func readQuestionFile() []QuestionLine {
