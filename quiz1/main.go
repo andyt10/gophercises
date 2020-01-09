53cafb0a1b5cd1a218aa0cba62823beb1ebdf802
Andy Thompson <andy@andythompson.com.au>
2020-01-09 19:20:11 +1100

    part2 done

--- quiz1/main.go
+++ quiz1/main.go
@@ -3,34 +3,58 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 
-	questionData := readQuestionFile()
-	totalMark := askQuestions(questionData)
+	runQuiz()
+}
+
+func runQuiz() {
+	fileName, limit := paseArgs()
+	questionData := readQuestionFile(fileName)
+	totalMark := askQuestions(questionData, limit)
 	fmt.Println("Total Marks:", totalMark)
 }
 
-func askQuestions(questionData []QuestionLine) int {
+func paseArgs() (string, int) {
+	fileName := flag.String("csv", "problems.csv", "A CSV File of Questions")
+	timeLimitSeconds := flag.Int("time", 30, "Time limit for quiz")
+	flag.Parse()
+	return *fileName, *timeLimitSeconds
+}
+
+func askQuestions(questionData []QuestionLine, timeLimitSeconds int) int {
+	timer1 := time.NewTimer(time.Duration(timeLimitSeconds) * time.Second)
+	//Quiz
 	totalMark := 0
 	reader := bufio.NewReader(os.Stdin)
 	for i := 0; i < len(questionData); i++ {
 		fmt.Println(fmt.Sprintf("What is: %v?", questionData[i].question))
-		answer, _ := reader.ReadString('\n')
-		answer = strings.Replace(answer, "\n", "", -1)
 
-		if answer == questionData[i].answer {
-			fmt.Println("Correct!")
-			totalMark += 1
-		} else {
-			fmt.Println("You Suck!")
-			fmt.Println(questionData[i].answer)
+		answerData := make(chan string)
+
+		go func() {
+			answer, _ := reader.ReadString('\n')
+			answer = strings.Replace(answer, "\n", "", -1)
+			answerData <- answer
+		}()
+
+		select {
+		case <-timer1.C:
+			fmt.Println("-- Out Of Time -- ")
+			return totalMark
+		case answer := <-answerData:
+			if answer == questionData[i].answer {
+				totalMark += 1
+			}
 		}
 	}
 
@@ -42,9 +66,15 @@ type QuestionLine struct {
 	answer   string
 }
 
-func readQuestionFile() []QuestionLine {
-	fileName := "problems.csv"
-	csvFile, _ := os.Open(fileName)
+func readQuestionFile(fileName string) []QuestionLine {
+
+	csvFile, err := os.Open(fileName)
+
+	if err != nil {
+		fmt.Println("Error Opening Questions File: ", err)
+		os.Exit(1)
+	}
+
 	reader := csv.NewReader(bufio.NewReader(csvFile))
 	var questionData []QuestionLine
 	for {
