9aaa3dece51a40a85e30256a957b38391cddebcc
Andy Thompson <andy@andythompson.com.au>
2020-01-11 15:52:07 +1100

    json parsing

--- adventure/main.go
+++ adventure/main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	server "cor_gophercises/adventure/pkg/bookserver"
+	"cor_gophercises/adventure/pkg/logger"
 	bookloader "cor_gophercises/adventure/pkg/storybook"
-	"fmt"
+	"flag"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
 	//1. Read Book
 	//2. Initialise Web Server
 	//3. Serve Book
-	fmt.Println()
+	logger.Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stdout)
+	logger.Info.Println("Starting CYOA book")
+
+	bookName := parseArgs()
+	logger.Info.Println("Using Book File:", bookName)
+
+	bookloader.LoadBook(bookName)
+
 	server.StartServer()
-	bookloader.LoadBook()
+
+}
+
+func parseArgs() string {
+	logger.Trace.Println("Parsing arguments")
+	fileName := flag.String("bookdata", "book_data.json", "A JSON file (to required spec) of a CYOA book.")
+
+	logger.Trace.Println("Arguments parsed")
+	return *fileName
 }
--- adventure/pkg/storybook/bookreader.go
+++ adventure/pkg/storybook/bookreader.go
@@ -1,5 +1,50 @@
 package storybook
 
-func LoadBook() {
+import (
+	"cor_gophercises/adventure/pkg/logger"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
+const firstPage = "intro"
+
+type BookFile map[string]BookPage
+
+type BookPage struct {
+	Title   string        `json:"title"`
+	Story   []string      `json:"story"`
+	Options []BookOptions `json:"options"`
+}
+
+type BookOptions struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+
+func LoadBook(fileName string) (error, *BookFile) {
+	var bookData BookFile
+
+	bookFile, fileReadError := os.Open(fileName)
+
+	if fileReadError != nil {
+		logger.Error.Println("Error opening book file:", fileName)
+		logger.Error.Println(fileReadError.Error())
+		return fileReadError, nil
+	}
+
+	bookFileData, _ := ioutil.ReadAll(bookFile)
+
+	jsonParseError := json.Unmarshal(bookFileData, &bookData)
+
+	if jsonParseError != nil {
+		logger.Error.Println("Error opening book file:", fileName)
+		logger.Error.Println(jsonParseError)
+		return jsonParseError, nil
+	}
+
+	fmt.Println(bookData)
+
+	return nil, nil
 }
