425d469d24a2fe5c2c5f9fd992dbc87c5389cc5f
Andy Thompson <andy@andythompson.com.au>
2020-02-21 11:20:53 +0000

    arg parsing

--- sitemap/main.go
+++ sitemap/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-	"cor_gophercises/sitemap/pkg/link"
+	"flag"
+	"fmt"
 )
 
 func main() {
 
-	link.OpenSource()
+	//	"cor_gophercises/sitemap/pkg/link"
+
+	//link.OpenSource()
+
+	site, depth := parseArgs()
+
+	fmt.Println(site, depth)
+}
+
+func parseArgs() (string, int) {
+
+	maxDepth := flag.Int("max-depth", 3, "Max number of links to follow in a site before stopping.")
+	siteName := flag.String("site-name", "", "A site to create a map for.")
+	flag.Parse()
+
+	return *siteName, *maxDepth
+
 }
